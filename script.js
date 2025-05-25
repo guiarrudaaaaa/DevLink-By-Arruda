fb3c902575b924793f9c80afd427bced445fc736
guiarrudaaaaa <arrudadesouzaaa@gmail.com>
2025-05-25 01:04:28 -0300

    Devlink by Arruda

---
+++ script.js
@@ -0,0 +1,13 @@
+function togglemode() {
+  const html = document.documentElement
+
+  // function newFunction() {
+  //   if (html.classList.contains("light")) {
+  //     html.classList.remove("light")
+  //   } else {
+  //     html.classList.add("light")
+  //   }
+  // }
+
+  html.classList.toggle("light")
+}
